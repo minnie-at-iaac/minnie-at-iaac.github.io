da21661d611f51ddddd8df8376ae97a013877a80
kotsengkuba <kotsengkuba@users.noreply.github.com>
2024-08-02 22:16:55 +0000

    deploy: 67048cb964a7c185601a9cbae9aa162e77b7944a

--- theme-picker.js
+++ theme-picker.js
@@ -1,61 +1,18 @@
 function changeTheme() {
-    docsifyDarkSwitcher();
+    docsifyThemeSwitcher();
 }
 
-function docsifyDarkSwitcher() {
+function docsifyThemeSwitcher() {
     var element = document.querySelector('body');
     element.classList.toggle("docsify-dark-mode");
 
     if (element.classList.contains("docsify-dark-mode")) {
         localStorage.setItem('docsify-dark-mode', 'true');
+        document.querySelector('#switch-theme-btn').textContent = "Ube Theme";
+        document.querySelector('#cover-logo').src = '/images/logo_minnie.png';
     } else {
         localStorage.removeItem('docsify-dark-mode');
+        document.querySelector('#switch-theme-btn').textContent = "Padan Theme";
+        document.querySelector('#cover-logo').src = '/images/logo_minnie_pandan.png';
     }
 }
-
-function applyInitialMode() {
-    if (localStorage.getItem('docsify-dark-mode') === 'true') {
-        document.querySelector('body').classList.add("docsify-dark-mode");
-    }
-}
-
-document.addEventListener('DOMContentLoaded', applyInitialMode);
-
-function styleInject(css, ref) {
-    if (ref === void 0) ref = {};
-    var insertAt = ref.insertAt;
-
-    if (!css || typeof document === 'undefined') {
-        return;
-    }
-
-    var head = document.head || document.getElementsByTagName('head')[0];
-    var style = document.createElement('style');
-    style.type = 'text/css';
-
-    if (insertAt === 'top') {
-        if (head.firstChild) {
-            head.insertBefore(style, head.firstChild);
-        } else {
-            head.appendChild(style);
-        }
-    } else {
-        head.appendChild(style);
-    }
-
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-}
-
-function install(hook, vm) {
-    hook.afterEach(function (html) {
-        //var docsifyDarkSwitcherButton = '<button onclick="docsifyDarkSwitcher()" aria-label="Toggle dark mode" title="Toggle dark mode"></button>';
-        //var docsifyDarkSwitcherButton = '<span>hello</span>'
-        //return docsifyDarkSwitcherButton + html;
-    });
-}
-
-//$docsify.plugins = [].concat(install, $docsify.plugins);
